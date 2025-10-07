96fefef28134a7257ee7ad4cf522978d70de8f85
Tamim <tamim.rauful@gmail.com>
2025-10-07 19:20:51 +0600

    lab task completed

--- lab4/lab task/task.py
+++ lab4/lab task/task.py
@@ -1,23 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 17 07:10:21 2025
-
-@author: USER
-"""
-
 import cv2
 import numpy as np
-from math import pi
+from math import pi, sqrt
 from tabulate import tabulate
-import math
-from math import sqrt
 
 # -------------------------------
 # Functions
 # -------------------------------
 
 def find_max_d(binary_image):
-    min_x = min_y = 1e9 
+    min_x = min_y = 1e9
+    max_x = max_y = 0
     h, w = binary_image.shape
     for x in range(h):
         for y in range(w):
@@ -33,8 +25,7 @@ def calculate_descriptors(binary_image):
     kernel = np.ones((3,3), np.uint8)
     eroded = cv2.erode(binary_image, kernel, iterations=1)
     border_image = binary_image - eroded
-    
-
+ 
     # Show images
     cv2.imshow("image - Binary", binary_image)
     cv2.imshow("image - Border", border_image)
@@ -50,50 +41,43 @@ def calculate_descriptors(binary_image):
     form_fact = (4 * pi * area) / (perimeter**2) if perimeter > 0 else 0
     roundness = (4 * area) / (pi * max_d**2) if max_d > 0 else 0
 
-    return np.array([form_fact, roundness, compact])  # order = [FF, R, C]
+    return (compact, form_fact, roundness)
 
-def cosine_similarity(v1, v2):
-    # dot product
-    dot = sum(v1[i] * v2[i] for i in range(len(v1)))
-    # manual norms
-    norm1 = sqrt(sum(v1[i]**2 for i in range(len(v1))))
-    norm2 = sqrt(sum(v2[i]**2 for i in range(len(v2))))
-    # cosine similarity
-    return dot / (norm1 * norm2) if norm1 > 0 and norm2 > 0 else 0
-
+def find_dist(t1, t2):
+    return sqrt((t1[0]-t2[0])**2 + (t1[1]-t2[1])**2 + (t1[2]-t2[2])**2)
 
 # -------------------------------
 # Main Code
 # -------------------------------
 
-image_name = ['c1.jpg','t1.jpg','p1.png','c2.jpg','t2.jpg','p2.png','st.jpg']
+image_name = ['c1.jpg','t1.jpg','p1.png','c2.jpg','t2.jpg','p2.png']
 
 descriptors = []
 for name in image_name:
     img_gray = cv2.imread(name, 0)
 
+
     descriptors.append(calculate_descriptors(img_gray))
 
 # Training descriptors (first 3)
 train = descriptors[:3]
-# Test descriptors (rest)
-test = descriptors[3:]
 
-# Cosine similarity matrix
-similarity = []
-for t in test:
-    row = [cosine_similarity(tr, t) for tr in train]
-    similarity.append(row)
+# Distance matrix for test images
+dist = []
+for i in range(3, len(image_name)):
+    desc = descriptors[i]
+    my_d = [find_dist(train[j], desc) for j in range(3)]
+    dist.append(my_d)
 
-# Print descriptors
-print("\n=== Shape Descriptors (FF, Roundness, Compactness) ===")
+# Print descriptors table
+print("\n=== Shape Descriptors ===")
 print(tabulate(descriptors,
-               headers=["Form Factor","Roundness","Compactness"],
+               headers=["Compactness","Form Factor","Roundness"],
                showindex=image_name,
                tablefmt="grid"))
 
-# Print similarity matrix
-print("\n=== Cosine Similarity Matrix ===")
+# Print distance matrix
+print("\n=== Distance Matrix ===")
 row_headers = image_name[3:]
 col_headers = image_name[:3]
-print(tabulate(similarity, headers=col_headers, showindex=row_headers, tablefmt='grid'))
+print(tabulate(dist, headers=col_headers, showindex=row_headers, tablefmt='grid'))
